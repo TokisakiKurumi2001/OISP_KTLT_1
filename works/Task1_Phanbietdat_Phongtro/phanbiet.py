614f304c60e92393936fb8a87bd586ee140cffde
Tommy <hoangle1001@outlook.com>
2020-05-25 22:04:42 +0700

    Preparation for coding

---
+++ works/Task1_Phanbietdat_Phongtro/phanbiet.py
@@ -0,0 +1,24 @@
+
+
+def phanbiet(dict_input: dict) -> int:
+    """Phân biệt cái gì đó ahihi, mọi người sửa lại dòng này cho đúng nhé.
+
+    Arguments:
+        inp_str {dict} -- dict chứa 3 thông tin sau:
+            id              : id của bài đăng
+            content         : content của bài đăng
+            realestate_type : loại bất động sản
+    Returns:
+        int - realestate_type mới của bài đăng
+    """
+
+    realestate_type = dict_input['realestate_type']
+
+    ## code goes here
+
+
+    ############################
+    ## Return
+    ############################
+    return realestate_type
+
---
+++ works/Task2_Phanbietdientich4/xacdinh.py
@@ -0,0 +1,27 @@
+
+
+def xacdinh(dict_input: dict) -> float:
+    """Phân biệt cái gì đó ahihi, mọi người sửa lại dòng này cho đúng nhé.
+
+    Arguments:
+        inp_str {dict} -- dict chứa 3 thông tin sau:
+            id              : id của bài đăng
+            content         : content của bài đăng
+            realestate_type : loại bất động sản
+            floor           : số tầng
+    Returns:
+        float - gía trị của biến area_cal
+
+    Như trong doc thì có 3 loại diện tích:
+    """
+
+    area_cal = 0
+
+    ## code goes here
+
+
+    ############################
+    ## Return
+    ############################
+    return area_cal
+
