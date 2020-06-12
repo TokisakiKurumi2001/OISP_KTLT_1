3984bc75ef285906f26fd09bf9faa69b20e554f9
Smithienious <nhoang3b@yahoo.com.vn>
2020-06-12 10:42:02 +0700

    First prototype

--- works/Task1_Phanbietdat_Nen/phanbiet.py
+++ works/Task1_Phanbietdat_Nen/phanbiet.py
@@ -1,7 +1,35 @@
+import re
+import json
+
+
+def remove_accents(input_str):
+    S1 = "ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ"
+    S0 = "AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy"
+
+    """Đổi các ký tự từ Unicode sang dạng không dấu và in thường
+
+    Arguments:
+        input_str {str} -- string cần chuyển đổi
+
+    Returns:
+        str -- string nếu chuyển đổi thành công
+        None - otherwise
+    """
+
+    if input_str is None:
+        return 'none'
+
+    s = ""
+    for c in input_str:
+        if c in S1:
+            s += S0[S1.index(c)]
+        else:
+            s += c
+    return s.lower()
 
 
 def phanbiet(dict_input: dict) -> int:
-    """Phân biệt cái gì đó ahihi, mọi người sửa lại dòng này cho đúng nhé.
+    """Phân biệt đất nền (10).
 
     Arguments:
         inp_str {dict} -- dict chứa 3 thông tin sau:
@@ -12,13 +40,30 @@ def phanbiet(dict_input: dict) -> int:
         int - realestate_type mới của bài đăng
     """
 
-    realestate_type = dict_input['realestate_type']
+    #realestate_type = dict_input['realestate_type']
+    pattern = "dat\s+(nen|du\s+an)|nen\s+dat\s+((?!tho\s+cu).*)|phan\s+lo|dat_nen"
+    # code goes here
+    for item_read in dict_input:
+        realestate_type = item_read['realestate_type']
+        if (realestate_type == 1):
+            dict_content = item_read['content']
+            dict_content = remove_accents(dict_content)
+            result = re.search(pattern, dict_content)
 
-    ## code goes here
+            if (result is not None):
+                realestate_type = 10
+                print(item_read['id'])
+
+        item_read['realestate_type'] = realestate_type
+
+############################
+# Return
+############################
+    # return realestate_type
 
 
-    ############################
-    ## Return
-    ############################
-    return realestate_type
-
+# For testing purposes
+if __name__ == '__main__':
+    with open('test.json') as json_file:
+        data = json.load(json_file)
+        phanbiet(data)
