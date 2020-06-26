bd865cddba19cc7933a330aaf8d28a6b2b59c15d
Smithienious <nhoang3b@yahoo.com.vn>
2020-06-26 20:28:52 +0700

    Initalized pattern

--- works/Task1_Phanbietdat_Nen/phanbiet.py
+++ works/Task1_Phanbietdat_Nen/phanbiet.py
@@ -1,31 +1,5 @@
 import re
-import json
-
-
-def remove_accents(input_str):
-    S1 = "ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ"
-    S0 = "AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy"
-
-    """Đổi các ký tự từ Unicode sang dạng không dấu và in thường
-
-    Arguments:
-        input_str {str} -- string cần chuyển đổi
-
-    Returns:
-        str -- string nếu chuyển đổi thành công
-        None - otherwise
-    """
-
-    if input_str is None:
-        return 'none'
-
-    s = ""
-    for c in input_str:
-        if c in S1:
-            s += S0[S1.index(c)]
-        else:
-            s += c
-    return s.lower()
+from utils import remove_accents
 
 
 def phanbiet(dict_input: dict) -> int:
@@ -40,30 +14,27 @@ def phanbiet(dict_input: dict) -> int:
         int - realestate_type mới của bài đăng
     """
 
-    #realestate_type = dict_input['realestate_type']
-    pattern = "dat\s+(nen|du\s+an)|nen\s+dat\s+((?!tho\s+cu).*)|phan\s+lo|dat_nen"
+    realestate_type = dict_input['realestate_type']
     # code goes here
-    for item_read in dict_input:
-        realestate_type = item_read['realestate_type']
-        if (realestate_type == 1):
-            dict_content = item_read['content']
-            dict_content = remove_accents(dict_content)
-            result = re.search(pattern, dict_content)
+    if (realestate_type == 1):
+        dict_content = dict_input['content']
+        dict_content = remove_accents(dict_content)
 
-            if (result is not None):
-                realestate_type = 10
-                print(item_read['id'])
+        pattern1 = "dat\s+nen\s|dat\s+nen\s+tho\s+cu|dat\s+du\s+an|dat\s+xay\s+dung|khu\s+du\s+an|nen\s+dat|phan\s+lo|dat_nen"
+        result1 = re.search(pattern1, dict_content)
 
-        item_read['realestate_type'] = realestate_type
+        """Test xem có chứa từ khoá của đất thổ cư và nông nghiệp không
+            
+        pattern2 = "len\s+tho\s+cu|len\s+100%\s+tho\s+cu|dat\s+tho\s+cu|dat\s+tc|dat\s+o\s+tai\s+(do\s+thi|nong\s+thon)|dat\s+trong\s+cay|dat\s+nong\s+nghiep|dat\s+lam\s+nghiep"
+        result2 = re.search(pattern2, dict_content)
+        if (result1 is not None):
+            print("{0} {1}".format(dict_input['id'], result2 is not None))
+		"""
+
+        if (result1 is not None):
+            realestate_type = 10
 
 ############################
 # Return
 ############################
-    # return realestate_type
-
-
-# For testing purposes
-if __name__ == '__main__':
-    with open('test.json') as json_file:
-        data = json.load(json_file)
-        phanbiet(data)
+    return realestate_type
